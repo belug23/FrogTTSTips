a0d8c64dfa611fc9a026cb5d82cf028e2543b79d
Alexandre <alexandre.lessard.ti@gmail.com>
2019-08-20 04:43:47 -0700

    Fixing a few things in Windows
    There was a few typos and wierd responses from the Bot

--- lib/frogtipsreader.py
+++ lib/frogtipsreader.py
@@ -10,12 +10,14 @@ clr.AddReference('System.Speech')
 from System.Speech.Synthesis import SpeechSynthesizer # pylint: disable=all; noqa .net system
 
 
+
 class FrogTipsReader(object):
     """ 
     Because I hate coding with only functions and using Global variables.
     Here is the FrogTipsReader, the frog tips to TTS reader.
     This will download FrogTips and and playthem with the .net Voice synthetyser.
     """
+    script_name = "Frog TTS Tips"
     config_file = "config.json"
     application_name = 'Frog Tips TTS reader'
     version = '1.0.0'
@@ -50,13 +52,13 @@ class FrogTipsReader(object):
         self.register()
     
     def get_auth_data(self):
-        return json.dumps({
+        return {
             'uuid': self.uuid,
             'comment':  '{} {}'.format(
                 self.application_name,
                 self.version
             )
-        })
+        }
  
     def register(self):
         # Prepare request
@@ -68,7 +70,7 @@ class FrogTipsReader(object):
         # Post request
         result = self.parent.PostRequest(url, headers, data, True)
         # Decode and save API key
-        response = json.loads(result)
+        response = json.loads(json.loads(result)['response'])
         self.phrase = response['phrase']
     
     def get_http_auth(self):
@@ -81,8 +83,8 @@ class FrogTipsReader(object):
             'Accept': 'application/json',
             'Authorization': 'Basic {}'.format(self.get_http_auth()),
         }
-        result = self.parent.GetRequest(url, headers)
-        self.tips = json.loads(result)
+        result = self.parent.GetRequest(url,headers)
+        self.tips = json.loads(json.loads(result)['response'])
 
     def loadSettings(self):
         """
@@ -90,7 +92,7 @@ class FrogTipsReader(object):
             If not present, set the settings to some default values
         """
         try:
-            with codecs.open(os.path.join(self.base_path, self.config_file), encoding='utf-8-sig', mode='r') as file:
+            with codecs.open(os.path.join(self.base_path, '..', self.config_file), encoding='utf-8-sig', mode='r') as file:
                 self.settings = json.load(file, encoding='utf-8-sig')
         except Exception:
             self.settings = {
@@ -119,11 +121,12 @@ class FrogTipsReader(object):
             Parse the data sent from the bot to see if we need to do something.
         """
         # If it's from chat and the live setting correspond to the live status
+        
         if self.canParseData(data):
             command = self.settings["command"].lower()
             if data.GetParam(0).lower() == command:
                 if self.hasPoints(data) and not self.isOnCoolDown(data, command):
-                    return self.playFrogTips(data)
+                    return self.playFrogTips(data, command)
         return
     
     def canParseData(self, data):
@@ -137,13 +140,13 @@ class FrogTipsReader(object):
     
     def hasPoints(self, data):
         points = self.parent.GetPoints(data.User)
-        return (point > self.settings['cost'])
+        return (points > self.settings['costs'])
     
     def isOnCoolDown(self, data, command):
         if (
             self.settings["useCooldown"] and
-            (self.parent.IsOnCooldown(ScriptName, command) or
-            self.parent.IsOnUserCooldown(ScriptName, command, data.User))
+            (self.parent.IsOnCooldown(self.script_name, command) or
+            self.parent.IsOnUserCooldown(self.script_name, command, data.User))
         ):
             self.sendOnCoolDownMessage(data, command)
             return True
@@ -152,8 +155,8 @@ class FrogTipsReader(object):
     
     def sendOnCoolDownMessage(self, data, command):
         if self.settings["useCooldownMessages"]:
-            commandCoolDownDuration = self.parent.GetCooldownDuration(ScriptName, command)
-            userCoolDownDuration = self.parent.GetUserCooldownDuration(ScriptName, command, data.User)
+            commandCoolDownDuration = self.parent.GetCooldownDuration(self.script_name, command)
+            userCoolDownDuration = self.parent.GetUserCooldownDuration(self.script_name, command, data.User)
 
             if commandCoolDownDuration > userCoolDownDuration:
                 cdi = commandCoolDownDuration
@@ -165,17 +168,17 @@ class FrogTipsReader(object):
             cd = str(cdi / 60) + ":" + str(cdi % 60).zfill(2) 
             self.sendMessage(data, message, command=command, cd=cd)
     
-    def playFrogTips(self, data):
+    def playFrogTips(self, data, command):
         if not self.tips:
             self.download_tips()
         tip = self.tips.pop()
         self.spk.Speak(tip['tip'])
-        self.setCoolDown(data)
+        self.setCoolDown(data, command)
 
     def setCoolDown(self, data, command):
         if self.settings["useCooldown"]:
-            self.parent.AddUserCooldown(ScriptName, command, data.User, self.settings["userCooldown"])
-            self.parent.AddCooldown(ScriptName, command, self.settings["cooldown"])
+            self.parent.AddUserCooldown(self.script_name, command, data.User, self.settings["userCooldown"])
+            self.parent.AddCooldown(self.script_name, command, self.settings["cooldown"])
 
 
     def sendMessage(self, data, message, command=None, cd="0"):
@@ -187,7 +190,6 @@ class FrogTipsReader(object):
             cost=str(None),  # not used
             currency=self.parent.GetCurrencyName(),
             command=command,
-            avail_sound_commands=self.avail_sound_commands,
             cd=cd
         )
         self.parent.SendStreamMessage(outputMessage)
